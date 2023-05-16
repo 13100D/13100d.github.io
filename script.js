ff1feb4d6c65ace6d13e6e79075b8455ab2a91fe
13100D <78998762+13100D@users.noreply.github.com>
2023-05-16 10:23:51 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let delay = 250; // Delay in milliseconds between each animation frame
 
 function animateText() {
   textElement.textContent = phrases[index];
-  if (index/phrases.length=1){
+  if (index/phrases.length>=1){
       index=index+5;
   }
   index = (index + 1) % phrases.length;
