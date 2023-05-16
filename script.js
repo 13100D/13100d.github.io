de47aa4ef8cd54a008c5a5c77130652e321df117
13100D <78998762+13100D@users.noreply.github.com>
2023-05-16 10:13:09 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const textElement = document.getElementById('animated-text');
-const phrases = ['S', 'Se', 'Seg','Segg','Seggs'];
+const phrases = ['S', 'Se', 'Seg','Segg','Seggs','Seggs','Seggs','Seggs.','Seggs..','Seggs...'];
 
 let index = 0;
 let delay = 250; // Delay in milliseconds between each animation frame
